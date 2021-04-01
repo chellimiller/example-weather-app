0b711b37ea61b039c200a39b5c43e6b26edc197a
Michelle Miller <chellimiller@users.noreply.github.com>
2021-04-01 09:07:00 -0700

    #6 Add development proxy

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,7 +3,7 @@ import { getRequestType, RequestType, sendAsset, sendPrivateAsset } from './util
 
 const app = express();
 
-const port = 3000;
+const port = 4000;
 
 app.get('/*', (request, response) => {
   const requestType = getRequestType(request.url);
