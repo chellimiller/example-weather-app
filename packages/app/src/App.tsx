654bd041692dbab229ffa39257f87013dcfdbc66
Michelle Miller <chellimiller@users.noreply.github.com>
2021-03-31 22:35:11 -0700

    #6 Add gradient backgrounds

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
 import { withWeatherApiConfig } from './context/WeatherApiConfigContext';
-import { Location } from './types';
-import CurrentWeatherDisplay from './ui/CurrentWeatherDisplay';
+import useCurrentWeather from './hooks/useCurrentWeather';
+import { Location, LocationType, ResultStatus, Weather } from './types';
 import LocationSearch from './ui/LocationSearch';
+import './App.css';
+
+const calculateBackgroundTemperature = (weather?: Weather) => {
+  if (!weather) return '';
+  // These are inexact and based on personal preference
+  // Freezing is pretty close to 0°C or 32°F
+  if (weather.temperature.feelsLike <= 274) return 'bg-freezing';
+  if (weather.temperature.feelsLike <= 284) return 'bg-cold';
+  if (weather.temperature.feelsLike <= 293) return 'bg-cool';
+  if (weather.temperature.feelsLike <= 300) return 'bg-moderate';
+  if (weather.temperature.feelsLike <= 307) return 'bg-warm';
+  return 'bg-hot';
+}
+
+const DEFAULT_LOCATION: Location = {
+  type: LocationType.CITY_NAME,
+  city: 'Scottsdale',
+}
 
 const App: React.FC = withWeatherApiConfig(() => {
   const [location, setLocation] = useState<Location>();
 
   const doUpdateLocation = (_e: unknown, value?: Location) => setLocation(value);
+  const weatherResult = useCurrentWeather(location || DEFAULT_LOCATION);
+  const weather = weatherResult.status === ResultStatus.DATA_LOAD ? weatherResult.data : undefined;
+
+  const bgClass = calculateBackgroundTemperature(weather);
 
   return (
-    <div>
+    <div className={['app', 'flex-center-content', bgClass].join(' ')}>
       <LocationSearch onSubmit={doUpdateLocation} />
-      {
-        (location) && (<CurrentWeatherDisplay location={location} />)
-      }
+      <div>
+        {weather && weather.temperature.feelsLike}
+      </div>
     </div>
   );
 });
